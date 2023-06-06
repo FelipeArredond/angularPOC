890cebafbfea17f35a6bd9d81b791a68ec3719ce
FelipeArredond <pipechiqui77@gmail.com>
2023-06-06 12:04:13 -0500

    All

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, asNativeElements } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'pocazuremaps';
+
+  map: any;
+
+  ngOnInit() {
+    this.map = L.map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; OpenStreetMap contributors',
+    }).addTo(this.map);
+  }
 }
