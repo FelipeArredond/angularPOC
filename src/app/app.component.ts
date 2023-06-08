0d7b81f7361176d5662d930f4a14fdf04a396136
FelipeArredond <pipechiqui77@gmail.com>
2023-06-08 09:54:46 -0500

    Map error fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,27 @@ import * as L from 'leaflet';
     templateUrl: './app.component.html',
     styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
 
-  map: any;
+  private map: any;
 
-  ngOnInit() {
-    this.map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; OpenStreetMap contributors',
-    }).addTo(this.map);
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 39.8282, -98.5795 ],
+      zoom: 3
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
   }
 }
