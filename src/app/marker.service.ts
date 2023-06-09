005b79c6869d48d9a3303e7d9e7bd0ce1edf8b95
Santiago Mesa Parra <86623637+SantiagoMesa0911@users.noreply.github.com>
2023-06-09 18:56:35 -0500

    Commit 4 part 1

--- src/app/marker.service.ts
+++ src/app/marker.service.ts
@@ -7,19 +7,31 @@ import * as L from 'leaflet';
 })
 export class MarkerService {
   capitals: string = '/assets/data/usa-capitals.geojson';
-
+  
   constructor(private http: HttpClient) {
   }
 
   makeCapitalMarkers(map: any): void {
     this.http.get(this.capitals).subscribe((res: any) => {
+      L.marker
+      
       for (const c of res.features) {
         const lon = c.geometry.coordinates[0];
         const lat = c.geometry.coordinates[1];
-        const circle = L.circleMarker([lat, lon]);
-      
-        circle.addTo(map);
+        const nombre = c.properties.state[0];
+        const circle = L.circleMarker([lat, lon],nombre);
+        circle.addTo(map).bindPopup(
+
+          `
+          <h4>CORDENADAS ACTUALES</h4>
+          <p> ${lon} y ${lat}
+          </p>
+          <button>Actualizar</button>
+          <button>Modificar</button>
+          `
+          );
       }
     });
   }
+
 }
